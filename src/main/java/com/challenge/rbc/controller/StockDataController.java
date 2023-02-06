72ef72c0ed272c064e1accdc784ecb64f6ef4991
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-02-06 23:52:40 +0100

    Fix bulk insert

--- src/main/java/com/challenge/rbc/controller/StockDataController.java
+++ src/main/java/com/challenge/rbc/controller/StockDataController.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/stock-data")
 public class StockDataController {
-
+    private static final Logger log = LoggerFactory.getLogger(StockDataController.class);
     @Autowired
     private StockDataService stockDataService;
 
@@ -31,24 +30,40 @@ public class StockDataController {
         if (file.isEmpty()) {
             return new ResponseEntity<>("File is empty", HttpStatus.BAD_REQUEST);
         }
+
         if (!clientId.equals("abc123")) {
             return new ResponseEntity<>("Invalid client Id", HttpStatus.UNAUTHORIZED);
         }
-        stockDataService.bulkInsert(file, clientId);
-        return new ResponseEntity<>("Data inserted successfully", HttpStatus.OK);
+
+        try {
+            stockDataService.bulkInsert(file, clientId);
+            return new ResponseEntity<>("Data inserted successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error inserting data for clientId: {}", clientId, e);
+            return new ResponseEntity<>("Error inserting data", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @GetMapping("/{symbol}")
-    public ResponseEntity<List<StockData>> getStockData(@RequestHeader("X-Client_Id") String clientId,
-                                                        @PathVariable("symbol") String symbol) {
-        List<StockData> stockData = stockDataService.getStockDataBySymbol(symbol, clientId);
-        if (stockData.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<?> getStockData(@RequestHeader("X-Client_Id") String clientId,
+                                          @PathVariable("symbol") String symbol) {
+        if (!clientId.equals("abc123")) {
+            return new ResponseEntity<>("Invalid client Id", HttpStatus.UNAUTHORIZED);
+        } else {
+            List<StockData> stockData = stockDataService.getStockDataBySymbol(symbol);
+            if (stockData.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(stockData, HttpStatus.OK);
         }
-        return new ResponseEntity<>(stockData, HttpStatus.OK);
     }
 
+
+    @GetMapping
+    public List<StockData> getDataByStockTicker(@RequestParam String stockTicker) {
+        return stockDataService.findDataByStockTicker(stockTicker);
+    }
 
 }
 
--- src/main/java/com/challenge/rbc/service/StockDataService.java
+++ src/main/java/com/challenge/rbc/service/StockDataService.java
@@ -4,10 +4,13 @@ import com.challenge.rbc.entity.StockData;
 import com.challenge.rbc.repository.StockDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,9 +18,9 @@ import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.stream.Stream;
 
 import java.text.ParseException;
+import java.util.stream.Collectors;
 
 @Service
 public class StockDataService {
@@ -38,46 +41,51 @@ public class StockDataService {
     }
 
 
-    public List<StockData> getStockDataBySymbol(String symbol, String clientId) {
+    public List<StockData> getStockDataBySymbol(String symbol) {
         return stockDataRepository.findByStock(symbol);
     }
 
+
     public List<StockData> bulkInsert(MultipartFile file, String clientId) {
         List<StockData> stockDataList = new ArrayList<>();
-        try (Stream<String> stream = Files.lines(Paths.get(file.getOriginalFilename()))) {
-            stream.forEach(line -> {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
-                StockData stockData = null;
-                try {
-                    stockData = StockData.builder()
-                            .quarter(Integer.parseInt(data[0]))
-                            .stock(data[1])
-
-                            .date(new SimpleDateFormat("MM/dd/yyyy").parse(data[2]))
-                            .open(Double.parseDouble(data[3]))
-                            .high(Double.parseDouble(data[4]))
-                            .low(Double.parseDouble(data[5]))
-                            .close(Double.parseDouble(data[6]))
-                            .volume(Integer.parseInt(data[7]))
-                            .percentChangePrice(Double.parseDouble(data[8]))
-                            .percentChangeVolumeOverLastWeek(Double.parseDouble(data[9]))
-                            .previousWeeksVolume(Integer.parseInt(data[10]))
-                            .nextWeeksOpen(Double.parseDouble(data[11]))
-                            .nextWeeksClose(Double.parseDouble(data[12]))
-                            .percentChangeNextWeeksPrice(Double.parseDouble(data[13]))
-                            .daysToNextDividend(Integer.parseInt(data[14]))
-                            .percentReturnNextDividend(Double.parseDouble(data[15]))
-                            .build();
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
-                }
+                StockData stockData = StockData.builder()
+                        .clientId(clientId)
+                        .quarter(Integer.parseInt(data[0]))
+                        .stock(data[1])
+                        .date(new SimpleDateFormat("MM/dd/yyyy").parse(data[2]))
+                        .open(Double.parseDouble(data[3].replace("$", "")))
+                        .high(Double.parseDouble(data[4].replace("$", "")))
+                        .low(Double.parseDouble(data[5].replace("$", "")))
+                        .close(Double.parseDouble(data[6].replace("$", "")))
+                        .volume(Integer.parseInt(data[7].replace("$", "")))
+                        .percentChangePrice(Double.parseDouble(data[8].replace("$", "")))
+                        .percentChangeVolumeOverLastWeek(Double.parseDouble((data[9].isEmpty() ? String.valueOf(0) : data[9])))
+                        .previousWeeksVolume(Integer.parseInt((data[10].isEmpty() ? String.valueOf(0) : data[10])))
+                        .nextWeeksOpen(Double.parseDouble(data[11].replace("$", "")))
+                        .nextWeeksClose(Double.parseDouble(data[12].replace("$", "")))
+                        .percentChangeNextWeeksPrice(Double.parseDouble(data[13]))
+                        .daysToNextDividend(Integer.parseInt(data[14]))
+                        .percentReturnNextDividend(Double.parseDouble(data[15]))
+                        .build();
                 stockDataList.add(stockData);
-            });
-        } catch (IOException e) {
+            }
+            stockDataRepository.saveAll(stockDataList);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        stockDataRepository.saveAll(stockDataList);
         return stockDataList;
     }
+
+
+    public List<StockData> findDataByStockTicker(String stockTicker) {
+        return stockDataRepository.findByStock(stockTicker);
+    }
+
 
 }
